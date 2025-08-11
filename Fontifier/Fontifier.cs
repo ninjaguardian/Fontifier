c85a0166c54ddb55ef9512f2e75d3771be61c378
ninjaguardian <cjfairchild51@gmail.com>
2025-08-11 18:55:03 -0500

    Add the attributes

--- Fontifier/Fontifier.cs
+++ Fontifier/Fontifier.cs
@@ -1,5 +1,15 @@
 ﻿using MelonLoader;
 
+[assembly: MelonInfo(typeof(Fontifier.Fontifier), "Fontifier", "1.0.0", "ninjaguardian", "https://thunderstore.io/c/rumble/p/ninjaguardian/Fontifier")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+[assembly: MelonColor(255, 0, 222, 230)]
+[assembly: MelonAuthorColor(255, 0, 160, 230)]
+
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
+[assembly: VerifyLoaderVersion(0, 7, 0, true)]
+
 namespace Fontifier
 {
     /// <summary>
