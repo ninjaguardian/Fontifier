455e5506891d5e9c58423137b4399c5ff197c9a7
ninjaguardian <cjfairchild51@gmail.com>
2025-08-11 21:45:34 -0500

    Fix names

--- Fontifier/Fontifier.cs
+++ Fontifier/Fontifier.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.TextCore.LowLevel;
+using System.Drawing.Text;
 
 [assembly: MelonInfo(typeof(Fontifier.Fontifier), "Fontifier", FontifierModInfo.ModVer, "ninjaguardian", "https://thunderstore.io/c/rumble/p/ninjaguardian/Fontifier")]
 [assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
@@ -49,9 +50,16 @@ namespace Fontifier
             Mod.ModName = "Fontifier";
             Mod.ModVersion = FontifierModInfo.ModVer;
             Mod.SetFolder("Fontifier");
-            Mod.AddDescription("Description", "Description", "Lets you change the font for other mods.", new Tags { IsSummary = true });
+            Mod.AddDescription("Description", "", "Lets you change the font for other mods.", new Tags { IsSummary = true });
             List<TMP_FontAsset> fonts = new();
-            foreach (string fontPath in Directory.GetFiles(@"UserData\Fontifier\fonts"))
+
+            foreach (string fontPath in
+                Directory.EnumerateFiles(@"UserData\Fontifier\fonts", "*.*", SearchOption.TopDirectoryOnly)
+                .Where(
+                    f => f.EndsWith(".ttf", StringComparison.OrdinalIgnoreCase)
+                    || f.EndsWith(".otf", StringComparison.OrdinalIgnoreCase)
+                )
+            )
             {
                 try
                 {
@@ -65,14 +73,29 @@ namespace Fontifier
                         1024
                     );
                     loadedFont.hideFlags = HideFlags.HideAndDontSave;
+
+                    try
+                    {
+                        using var fontCollection = new PrivateFontCollection();
+                        fontCollection.AddFontFile(fontPath);
+                        if (fontCollection.Families.Length > 0)
+                            loadedFont.name = fontCollection.Families[0].Name;
+                    }
+                    catch (Exception ex)
+                    {
+                        loadedFont.name = Path.GetFileNameWithoutExtension(fontPath);
+                        MelonLogger.Warning($"Could not read internal font name for {fontPath}, using filename instead.", ex);
+                    }
+
                     fonts.Add(loadedFont);
+                    MelonLogger.Msg($"Loaded font: {loadedFont.name} from {fontPath}");
                 }
                 catch (Exception ex)
                 {
                     MelonLogger.Error($"Failed to load font from {fontPath}", ex);
                 }
             }
-            Mod.AddDescription("Fonts list", "Fonts list", string.Join("\n", fonts.Select(f => f.name)), new Tags { IsSummary = true });
+            Mod.AddDescription("Fonts List", "", "The following fonts are loaded:\n" + string.Join("\n", fonts.Select(f => f.name)), new Tags { IsEmpty = true });
 
             Mod.GetFromFile();
             UI.instance.AddMod(Mod);
