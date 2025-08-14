662dce167480b9547899bf513ed21db0c7d2352d
ninjaguardian <cjfairchild51@gmail.com>
2025-08-13 22:51:04 -0500

    New system

--- Fontifier/Fontifier.cs
+++ Fontifier/Fontifier.cs
@@ -271,12 +271,12 @@ namespace Fontifier
         /// <param name="modName">The name of your MelonMod (this.Info.Name)</param>
         /// <param name="valueChanged">The function that will be ran when the value changes</param>
         /// <returns>Invoke to get the current TMP_FontAsset</returns>
-        public static Func<TMP_FontAsset> RegisterMod(string modName, EventHandler<EventArgs> valueChanged)
+        public static (Func<TMP_FontAsset>, Func<string, TMP_FontAsset>) RegisterMod(string modName, EventHandler<EventArgs> valueChanged)
         {
             ModSetting<string> setting = ModUI.AddToList(modName, "", ModDesc, new Tags());
             ModUI.AddValidation(modName, validator);
             setting.CurrentValueChanged += valueChanged;
-            return () => FontFromName((string)setting.Value);
+            return (() => FontFromName((string)setting.Value), FontFromName);
         }
     }
 }
